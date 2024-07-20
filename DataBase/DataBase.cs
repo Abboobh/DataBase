8a2f3c44488bc5a2d5f9a4f579c4bc143b14a3c4
Уткин Владимир <VladimirUtkin4308@yandex.ru>
2024-07-20 21:26:37 +0300

    ver 0.1

--- DataBase/DataBase.cs
+++ DataBase/DataBase.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataBase
+﻿namespace DataBase
 {
     internal class DataBase
     {
+        public const int TABLINE = 110;
         private class Order
         {
             public Order()
@@ -33,10 +27,16 @@ namespace DataBase
             public string _name_orderer = "";
             public int _id = 0;
 
-            public List<int> _price = new List<int>();
+            public List<Tuple<int, int>> _price = new List<Tuple<int, int>>();
             public void AddPrice(int price)
             {
-                _price.Add(price);
+                int id_price = _price.Count;
+                _price.Add(new Tuple<int, int>(id_price, price));
+
+            }
+            public void RemovePrice(int index)
+            {
+                _price.RemoveAt(index);
             }
             public int SumPrice()
             {
@@ -44,7 +44,7 @@ namespace DataBase
 
                 for (int i = 0; i < _price.Count; i++)
                 {
-                    result += _price[i];
+                    result += _price[i].Item2;
                 }
 
                 return result;
@@ -53,9 +53,28 @@ namespace DataBase
 
         private List<Order> _orders = new List<Order>();
         private static int _count;
-
+        /*--------------Методы упрощения---------------*/
+        private static void PrintHat()
+        {
+            Console.WriteLine("[ID]\t\t\t\t[Order]\t\t\t\t[Orderer]\t\t\t[Sum price]");
+            for (int tab = 0; tab < TABLINE; tab++)
+            {
+                Console.Write("_");
+            }
+            Console.WriteLine();
+        }
+        private static string PrintBody(Order order)
+        {
+            return "[" + order._id + "]\t\t\t\t[" + order._name_order + "]\t\t\t["
+                       + order._name_orderer + "]\t\t\t\t[" + order.SumPrice() + "]";
+        }
         /*-------------Методы манипуляциии-------------*/
-        public void AddOrder(string name_order, string name_orderer)
+        /// <summary>
+        /// Добавить заказ в базу данных
+        /// </summary>
+        /// <param name="name_order">Номер заказа</param>
+        /// <param name="name_orderer">Наименование заказчика</param>
+        public void AddOrder(string name_order, string name_orderer) 
         {
             Order order = new Order(name_order, name_orderer);
             _orders.Add(order);
@@ -63,6 +82,30 @@ namespace DataBase
         public void AddPrice(int index, int price)
         {
             _orders[index].AddPrice(price);
+        }
+        public void AddPrice(string order, int price)
+        {
+            foreach (var item in _orders)
+            {
+                if (item._name_order == order)
+                {
+                    AddPrice(item._id, price);
+                }
+            }
+        }
+        public void RemovePrice(int index_order, int index_price)
+        {
+            _orders[index_order].RemovePrice(index_price);
+        }
+        public void RemovePrice(string order_name, int index_price)
+        {
+            foreach (var item in _orders)
+            {
+                if (item._name_order == order_name)
+                {
+                    _orders[item._id].RemovePrice(index_price);
+                }
+            }
         }
         public int SumPrice(int index)
         {
@@ -72,29 +115,50 @@ namespace DataBase
         /*-------------Методы информации--------------*/
         public string GetOrderInfo(int index)
         {
-            return _orders[index]._id + " " + _orders[index]._name_order + " " + _orders[index]._name_orderer + " " + _orders[index].SumPrice();
+            PrintHat();
+            return PrintBody(_orders[index]);
         }
         public string GetOrderInfo(string order)
         {
-            List<Order> sortedlist = _orders;
-
-            sortedlist.Sort();
-
-            int result = _orders.BinarySearch(new Order { _name_order = order });
-
-            return sortedlist[result]._id + " " + sortedlist[result]._name_order + " " 
-                 + sortedlist[result]._name_orderer + " " + sortedlist[result].SumPrice();
+            foreach (var item in _orders)
+            {
+                if(item._name_order == order)
+                {
+                   return GetOrderInfo(item._id);
+                }
+            }
+            return "Order not found";
         }
-        public void PrintInfo()
+        public void PrintOrders()
         {
+            PrintHat();
             for (int index = 0; index < _count; index++)
             {
-                Console.WriteLine(_orders[index]._id + " " + _orders[index]._name_order + " " 
-                                + _orders[index]._name_orderer + " " + _orders[index].SumPrice());
+                Console.WriteLine(PrintBody(_orders[index]));
+            }
+            Console.WriteLine();
+        }
+        public void PrintOrderInfo(int index)
+        {
+            PrintHat();
+            Console.WriteLine("[" + _orders[index]._id + "]\t\t\t\t[" + _orders[index]._name_order + "]\t\t\t[" + _orders[index]._name_orderer + "]");
+            for (int index2 = 0; index2 <= _orders[index]._price.Count - 1; index2++)
+            { 
+                Console.WriteLine("\t\t\t\t\t\t\t\t\t\t\t\t[" + _orders[index]._price[index2] + "]");
+            }
+        }
+        public void PrintOrderInfo(string order)
+        {
+            foreach (var item in _orders)
+            {
+                if (item._name_order == order)
+                {
+                    PrintOrderInfo(item._id);
+                }
             }
         }
     }
-    static class Method
+    static class General
     {
 
     }
--- DataBase/Program.cs
+++ DataBase/Program.cs
@@ -1,17 +1,128 @@
-﻿namespace DataBase
+﻿using System.ComponentModel.Design;
+using System.Text.Json;
+
+namespace DataBase
 {
     internal class Program
     {
+        public static void Print()
+        {
+            Console.WriteLine("Введите команду:\n" +
+                   "1 - Добавить заказ\n" +
+                   "2 - Добавить цену за раскрой в заказ\n" +
+                   "3 - Просмотреть заказ" +
+                   "4 - Удалить цену за раскрой из заказа");
+        }
+        public static void AddOrder(ref DataBase dataBase)
+        {
+            string name_order;
+            string name_orderer;
+            Console.WriteLine("Введите номер заказа");
+            name_order = Console.ReadLine();
+            Console.WriteLine("Введите наименование заказчика");
+            name_orderer = Console.ReadLine();
+            dataBase.AddOrder(name_order, name_orderer);
+        }
+        public static void AddPrice(ref DataBase dataBase)
+        {
+            bool flag = true;
+            int price;
+            Console.WriteLine("Введите номер заказа");
+            string name_order = Console.ReadLine();
+            while (flag)
+            {
+                Console.WriteLine("Введите выработку за лист");
+                price = Convert.ToInt32(Console.ReadLine());
+                if (price > 0)
+                {
+                    dataBase.AddPrice(name_order, price);
+                }
+                else
+                {
+                    Console.WriteLine("Ценник не может быть отрицательный");
+                    continue;
+                }
+                Console.WriteLine("Ввести еще выработку\n 1 - да;\n2 - нет.");
+                string continue_cmd = Console.ReadLine();
+                if (continue_cmd == "2")
+                {
+                    flag = false;
+                }
+                else if (continue_cmd == "1")
+                {
+                    Console.Clear();
+                    dataBase.PrintOrders();
+                }
+                else 
+                {
+                    Console.WriteLine("Нет такой команды");
+                    continue;
+                }
+
+            }
+        }
+        public static void RemovePrice(ref DataBase dataBase)
+        {
+            Console.WriteLine("Введите номер заказа");
+            string name_order = Console.ReadLine();
+            Console.Clear();
+            dataBase.PrintOrderInfo(name_order);
+            Console.WriteLine("Введите номер цены за раскрой");
+            int index_price = Convert.ToInt32(Console.ReadLine());
+            dataBase.RemovePrice(name_order, index_price);
+        }
+        public static void PrintOrder(ref DataBase dataBase)
+        {
+
+            Console.WriteLine("Введите номер заказа");
+            string name_order = Console.ReadLine();
+            Console.Clear();
+            dataBase.PrintOrderInfo(name_order);
+            Console.WriteLine("Для продолжения нажмите любую кнопку");
+            Console.ReadKey();
+        }
+        public static void TestDataBase(ref DataBase dataBase)
+        {
+            dataBase.AddOrder("ЛУ-115", "Усов");
+            dataBase.AddPrice("ЛУ-115", 614);
+            dataBase.AddPrice("ЛУ-115", 543);
+            dataBase.AddOrder("ЛУ-123", "Верон");
+            dataBase.AddPrice("ЛУ-123", 1144);
+            dataBase.AddPrice("ЛУ-123", 5456);
+        }
         static void Main(string[] args)
         {
             DataBase dataBase = new DataBase();
+            TestDataBase(ref dataBase);
+            while (true) 
+            {
+                dataBase.PrintOrders();
+                int command = 0;
+                Print();
+                command = Convert.ToInt32(Console.ReadLine());
+                switch (command)
+                {
+                    case 1:
+                        AddOrder(ref dataBase);
+                        break;
 
-            dataBase.AddOrder("ЛУ-190", "Усов");
-            dataBase.AddOrder("ЛУ-191", "Писос");
-            dataBase.AddOrder("ЛУ-192", "Чюля");
-            dataBase.AddOrder("ЛУ-193", "Коч");
+                    case 2:
+                        AddPrice(ref dataBase);
+                        break;
 
-            dataBase.GetOrderInfo("ЛУ-193");
+                    case 3:
+                        PrintOrder(ref dataBase);
+                        break;
+
+                    case 4:
+                        RemovePrice(ref dataBase);
+                        break;
+                    default:
+                        Console.WriteLine("Команды с таким номером не существует");
+                        break;
+                }
+                Console.Clear();
+            }
         }
     }
 }
